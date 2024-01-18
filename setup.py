81ebdc6fa4d50687f48990d7a1049a0578ab0d79
MoraaOntita <moraaontita1844@gmail.com>
2024-01-18 03:24:52 +0300

    Imported my data and Begining my EDA

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='using machine learning to predict which passanger survived the Titanic shipwreck',
+    author='Moraa',
+    license='',
+)
